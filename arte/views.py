dbca9b50331cf4bd0e788074b4e0a0b09b0ef8fd
Luiz-aluisio <128824788+Luiz-aluisio@users.noreply.github.com>
2023-11-27 21:45:06 -0300

    criados endpoints de obra (#11)

--- arte/views.py
+++ arte/views.py
@@ -1,10 +1,16 @@
 from rest_framework import permissions, viewsets
 
-from .models import Autor
-from .serializers import AutorSerializer
+from .models import Autor, Obra
+from .serializers import AutorSerializer, ObraSerializer
 
 
 class AutorViewSet(viewsets.ModelViewSet):
     queryset = Autor.objects.all()
     serializer_class = AutorSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class ObraViewSet(viewsets.ModelViewSet):
+    queryset = Obra.objects.all()
+    serializer_class = ObraSerializer
     permission_classes = [permissions.IsAuthenticated]
